1c55c975dd71842c6a5a548dcd8dc12da8996bd4
Théo Schaffner <theoschaff@gmail.com>
2026-03-26 00:24:57 +0100

    feat(squish): add squishyness to the player and jump particles

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@export var animation_player : AnimationPlayer
+@export var particles : GPUParticles2D
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -11,6 +14,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		_animations()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -21,3 +25,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _animations() -> void:
+	if animation_player.has_animation("jump"):
+		animation_player.play("jump")
+	else:
+		print_debug("No jump animation found")
+	
+	if particles:
+		particles.restart()
+	else:
+		print_debug("No particles found")
