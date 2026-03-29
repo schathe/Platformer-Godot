380d2826f117146f5f59b0953ddf747267389776
Théo Schaffner <theoschaff@gmail.com>
2026-03-29 13:19:54 +0200

    feat(colors): add a color palette with a script

---
+++ assets/colors.gd
@@ -0,0 +1,13 @@
+extends Resource
+class_name PlatformerColors
+
+@export var normal: Color = Color.WHITE
+@export var destructible_warning: Color = Color.YELLOW
+@export var destructible_danger: Color = Color.RED
+@export var one_way: Color = Color(0.5, 0.8, 1.0)
+@export var moving: Color = Color(0.8, 0.8, 0.2)
+@export var ice: Color = Color(0.7, 0.9, 1.0)
+@export var bouncy: Color = Color(1.0, 0.6, 0.6)
+@export var locked: Color = Color.GRAY
+@export var water: Color = Color(0.3, 0.6, 1.0)
+@export var lava: Color = Color(1.0, 0.4, 0.0)
